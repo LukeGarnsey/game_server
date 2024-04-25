6b5e7add9574c09126b2eafe1ef7ba38f5619087
Luke Garnsey <lukegarnsey@gmail.com>
2024-04-25 13:05:32 -0400

    fix for env path

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const chatServer = require("./chat/chatServer");
-
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../client/dist')));
